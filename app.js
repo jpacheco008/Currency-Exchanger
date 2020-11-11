cc37df555ef0828f750b327390367be9532fcdc9
jpacheco008 <69394703+jpacheco008@users.noreply.github.com>
2020-11-11 09:19:10 -0500

    working dropdown

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log("connected");
+console.log('hello');
+const getRates = async () => {
+  const url = 'https://api.frankfurter.app/latest'
+  try {
+    const response = await axios.get(url)
+    // console.log(response.data.rates);
+    const rates = Object.keys(response.data.rates)
+    console.log(rates);
+    rateOptions(rates)
+  } catch (error) {
+    console.log(error);
+  }
+}
+getRates()
+
+function rateOptions(list) {
+  const select = document.querySelector('#current-currency')
+  return list.forEach((currency) => {
+    const option = document.createElement('option')
+    console.log(currency);
+    option.value = `${currency}`
+    option.textContent = `${currency}`
+    select.append(option)
+  });
+}
+rateOptions()
