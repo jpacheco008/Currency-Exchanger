5e6c222872c2ee0665579ff5d0f97e581e06f126
jpacheco008 <69394703+jpacheco008@users.noreply.github.com>
2020-11-12 13:08:53 -0500

    polish dropdown menus

--- app.js
+++ app.js
@@ -1,26 +1,62 @@
-console.log('hello');
+let rateValue;
 const getRates = async () => {
   const url = 'https://api.frankfurter.app/latest'
   try {
     const response = await axios.get(url)
     // console.log(response.data.rates);
     const rates = Object.keys(response.data.rates)
+    rates.splice(8, 0, response.data.base)
+    rateValue = response.data.rates
+    console.log(rateValue);
     console.log(rates);
-    rateOptions(rates)
+    currentOptions(rates)
+    newOptions(rates)
+    console.log(rateValue.AUD);
   } catch (error) {
     console.log(error);
   }
 }
 getRates()
 
-function rateOptions(list) {
-  const select = document.querySelector('#current-currency')
-  return list.forEach((currency) => {
+function currentOptions(list) {
+  const currentC = document.querySelector('#current-currency')
+  return list.forEach(currency => {
     const option = document.createElement('option')
-    console.log(currency);
     option.value = `${currency}`
     option.textContent = `${currency}`
-    select.append(option)
+    currentC.append(option)
   });
 }
-rateOptions()
+function newOptions(list) {
+  const newC = document.querySelector('#new-currency')
+  return list.forEach(currency => {
+    const optionNew = document.createElement('option')
+    optionNew.value = `${currency}`
+    optionNew.textContent = `${currency}`
+    newC.append(optionNew)
+  });
+}
+function currentCurrency(event) {
+  event.preventDefault()
+  const currentRateValue = document.querySelector('#current-currency').value
+  console.log(currentRateValue);
+  getCountry(currentRateValue)
+}
+
+
+async function getCountry(country) {
+  const url = `https://api.frankfurter.app/latest?from=${country}`
+  try {
+    const response = await axios.get(url)
+    console.log(response);
+
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const selectC = document.querySelector('#current-currency');
+console.log(selectC)
+selectC.addEventListener('change', (event) => {
+  getCountry(event.target.value)
+})
