43d91cdc988f4adc745c0f60975fc6de0369c237
jpacheco008 <69394703+jpacheco008@users.noreply.github.com>
2020-11-13 08:38:08 -0500

    working MVP

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 let rateValue;
+
 const getRates = async () => {
   const url = 'https://api.frankfurter.app/latest'
   try {
     const response = await axios.get(url)
-    // console.log(response.data.rates);
     const rates = Object.keys(response.data.rates)
     rates.splice(8, 0, response.data.base)
+    rates.splice(0,0, 'Choose currency')
     rateValue = response.data.rates
-    console.log(rateValue);
+    // console.log(rateValue.AUD);
     console.log(rates);
     currentOptions(rates)
     newOptions(rates)
-    console.log(rateValue.AUD);
+    
+    // console.log(rateValue.getCountry);
   } catch (error) {
     console.log(error);
   }
@@ -28,7 +30,7 @@ function currentOptions(list) {
   });
 }
 function newOptions(list) {
-  const newC = document.querySelector('#new-currency')
+  const newC = document.querySelector('#newCurrency')
   return list.forEach(currency => {
     const optionNew = document.createElement('option')
     optionNew.value = `${currency}`
@@ -36,27 +38,42 @@ function newOptions(list) {
     newC.append(optionNew)
   });
 }
-function currentCurrency(event) {
-  event.preventDefault()
-  const currentRateValue = document.querySelector('#current-currency').value
-  console.log(currentRateValue);
-  getCountry(currentRateValue)
-}
-
-
-async function getCountry(country) {
+let mainCountry = ''
+async function getCountry(country, exchangeCountry) {
   const url = `https://api.frankfurter.app/latest?from=${country}`
   try {
     const response = await axios.get(url)
     console.log(response);
-
+    let currentCountry = response.data.base
+    console.log(currentCountry);
+    console.log(exchangeCountry);
+    let rates = response.data.rates
+    for (const key in rates) {
+      if (key === exchangeCountry) {
+        newR = rates[key]
+        console.log(newR);
+        let money = document.querySelector('input');
+        money.addEventListener('change', (event) => {
+          let moneyAmnt = document.querySelector('input').value
+          console.log(moneyAmnt);
+          let newAmount = document.querySelector('#newAmount')
+          newAmount.value = `${moneyAmnt * newR}`
+          newAmount.textContent = `${moneyAmnt * newR}`
+          console.log(newAmount);
+        })
+      }
+    }
   } catch (error) {
     console.log(error);
   }
 }
 
 const selectC = document.querySelector('#current-currency');
-console.log(selectC)
 selectC.addEventListener('change', (event) => {
-  getCountry(event.target.value)
+  mainCountry = event.target.value
+})
+
+const selectN = document.querySelector('#newCurrency');
+selectN.addEventListener('change', (event) => {
+  getCountry(mainCountry, event.target.value)
 })
